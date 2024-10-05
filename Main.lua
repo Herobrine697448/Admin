050ff8c6de7f60d14b90cb851ef2036ef33cd0bf
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-05 18:16:09 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,32 @@
---NOTIFICATION
+--NOTIFICATION 1
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "NOTIFICATION",
+    Text = "Made by @Herobrine (FE)",
+    Duration = 10
+})
+
+--NOTIFICATION 2
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "NOTIFICATION",
+    Text = "Cant Be Detected By Anticheat (FE)",
+    Duration = 10
+})
+
+
+
+-- ULTIMATE TEXT
+local plr = game.Players.LocalPlayer
+local gui = plr.PlayerGui
+local ulttext = gui.ScreenGui.MagicHealth.TextLabel
+ulttext.Text = "HEAVENLY PUNISHMENT"
+
+
 
 --ADMIN KILLER CHARACTER M1 CHANGE (FE)
+
+
 
 --BACKPACK CLEAR
 -- Clear the player's backpack at the start
