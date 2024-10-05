6427f6f5f58ea2bcdf9b567c62a62db633c3b9c0
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-05 22:02:22 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,6 +29,6 @@ end
 clearBackpack()
 
 -- Load scripts
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Basic%20Moves/The%20Unwanted%20Risk.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Awakening/Awakening%20of%20the%20gods.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Basic%20Moves/Ravaging%20Deaths.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Basic%20Moves/The%20Unwanted%20Risk.lua'))()
