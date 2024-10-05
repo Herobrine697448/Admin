ee535f7c3f4706084389450ea2c105d0bfdad62a
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-05 19:09:03 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,15 +16,21 @@ StarterGui:SetCore("SendNotification", {
 local ulttext = gui.ScreenGui.MagicHealth.TextLabel
 ulttext.Text = "HEAVENLY PUNISHMENT"
 
+--BACKPACK CLEAR
+local function clearBackpack(player)
+    local backpack = player:WaitForChild("Backpack")
+
+    -- Clear all tools from the backpack
+    for _, item in pairs(backpack:GetChildren()) do
+        if item:IsA("Tool") then
+            item:Destroy() -- Remove the tool
+        end
+    end
+end
+
+clearBackpack(player) 
+
 -- Load scripts first
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Awakening/Awakening%20of%20the%20gods.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Basic%20Moves/Ravaging%20Deaths.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Herobrine697448/Admin/refs/heads/main/Basic%20Moves/The%20Unwanted%20Risk.lua'))()
-
--- BACKPACK CLEAR
-wait(0.5) -- Optional wait to ensure scripts are loaded
-for _, item in pairs(backpack:GetChildren()) do
-    if item:IsA("Tool") then
-        item:Destroy() -- Remove all tools from the backpack
-    end
-end
