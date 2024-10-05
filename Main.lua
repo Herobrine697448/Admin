b7f06679d2fae55d19af293472487b9989e6258c
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-05 18:12:45 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,6 +3,14 @@
 --ADMIN KILLER CHARACTER M1 CHANGE (FE)
 
 --BACKPACK CLEAR
+-- Clear the player's backpack at the start
+for _, item in pairs(backpack:GetChildren()) do
+    if item:IsA("Tool") then
+        item:Destroy() -- Remove all tools from the backpack
+    end
+end
+
+
 
 --AWAKENING OF THE GODS
 
